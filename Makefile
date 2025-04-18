76e0730af3d6cd05f4ccda5bad9c70456a969bcf
Cheng Haohan <haohan.cheng@shopee.com>
2025-04-18 15:10:45 +0800

    update the xargs

--- Makefile
+++ Makefile
@@ -196,6 +196,7 @@ UPROGS=\
 	$U/_usertests\
 	$U/_grind\
 	$U/_wc\
+	$U/_x_args\
 	$U/_zombie\
 
 
---
+++ user/xargs.c
@@ -0,0 +1,59 @@
+
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+#define MAX_SIZE 1024
+int main(int argc, char *argv[])
+
+{
+
+  char *child_argv[20]; // store the previous argv.
+  char *p;
+  char buf[MAX_SIZE];
+  int pid = fork();
+
+  // c
+  int pointer; // record the inital pointer here
+  for (pointer = 0; pointer < argc - 1; pointer++)
+  {
+    child_argv[pointer] = argv[pointer + 1];
+  }
+
+  int anchor = pointer;
+  int previous_start = 0;
+  int n = read(0, buf, sizeof(buf));
+  for (int i = 0; i < MAX_SIZE; i++)
+  {
+    if (buf[i] != ' ' && buf[i] != '\n')
+    {
+      continue;
+    }
+
+    else if (buf[i] == ' ')
+    {
+      buf[i] = "\0";
+      child_argv[pointer++] = &buf[previous_start];
+      previous_start = i + 1;
+    }
+
+    else
+    { // buf[i] == '\n'
+      child_argv[pointer] = 0;
+      int pid = fork();
+      if (pid == 0)
+      { // child
+
+        exec(argv[1], child_argv);
+        exit(0);
+      }
+      else
+      {
+        wait(0);
+        // to do reset child_argv
+        pointer = anchor;
+      }
+    }
+  }
+  exit(0);
+}
